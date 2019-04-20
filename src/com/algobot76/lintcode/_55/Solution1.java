625b8f343246d51e7f51ff37950a2e1505bd768a
Kaitian Xie <xkaitian@gmail.com>
2019-04-19 17:18:01 -0700

    55: Begin problem

---
+++ src/com/algobot76/lintcode/_55/Solution1.java
@@ -0,0 +1,12 @@
+package com.algobot76.lintcode._55;
+
+public class Solution1 {
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: if string A contains all of the characters in B return true else return false
+     */
+    public boolean compareStrings(String A, String B) {
+        // write your code here
+    }
+}
