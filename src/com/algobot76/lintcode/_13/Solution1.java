9f1017d1b02764963cd150773d76e28c6fdffa66
Kaitian Xie <xkaitian@gmail.com>
2019-04-20 15:52:05 -0700

    13: Begin problem

---
+++ src/com/algobot76/lintcode/_13/Solution1.java
@@ -0,0 +1,12 @@
+package com.algobot76.lintcode._13;
+
+public class Solution1 {
+    /**
+     * @param source:
+     * @param target:
+     * @return: return the index
+     */
+    public int strStr(String source, String target) {
+        // Write your code here
+    }
+}
