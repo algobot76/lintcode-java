8254c2a4facd7c6b04d3d52c2427900d7aecd1ea
Kaitian Xie <xkaitian@gmail.com>
2019-04-21 15:36:48 -0700

    171: finish problem

--- src/com/algobot76/lintcode/_171/Solution1.java
+++ src/com/algobot76/lintcode/_171/Solution1.java
@@ -1,6 +1,6 @@
 package com.algobot76.lintcode._171;
 
-import java.util.List;
+import java.util.*;
 
 public class Solution1 {
     /**
@@ -9,6 +9,45 @@ public class Solution1 {
      */
     public List<String> anagrams(String[] strs) {
         // write your code here
+        List<String> ans = new ArrayList<>();
+        if (strs == null || strs.length == 0) {
+            return ans;
+        }
+        Map<String, List<String>> map = new HashMap<>();
 
+        for (String str : strs) {
+            if (str.isEmpty()) {
+                String key = "#";
+                if (!map.containsKey(key)) {
+                    map.put(key, new ArrayList<>(Arrays.asList("")));
+                } else {
+                    map.get(key).add("");
+                }
+            } else {
+                int[] count = new int[26];
+                for (char ch : str.toCharArray()) {
+                    count[ch - 'a']++;
+                }
+                StringBuilder sb = new StringBuilder();
+                for (int c : count) {
+                    sb.append("#");
+                    sb.append(c);
+                }
+                String key = sb.toString();
+                if (!map.containsKey(key)) {
+                    map.put(key, new ArrayList<>(Arrays.asList(str)));
+                } else {
+                    map.get(key).add(str);
+                }
+            }
+        }
+
+        for (List<String> list : map.values()) {
+            if (list.size() >= 2) {
+                ans.addAll(list);
+            }
+        }
+
+        return ans;
     }
 }
