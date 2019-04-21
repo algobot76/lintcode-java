20a3a671f4d6406646353c8e0045faf352b3c02b
Kaitian Xie <xkaitian@gmail.com>
2019-04-21 11:13:22 -0700

    465: Begin problem

---
+++ src/com/algobot76/lintcode/_465/Solution1.java
@@ -0,0 +1,13 @@
+package com.algobot76.lintcode._465;
+
+public class Solution1 {
+    /**
+     * @param A: an integer arrays sorted in ascending order
+     * @param B: an integer arrays sorted in ascending order
+     * @param k: An integer
+     * @return: An integer
+     */
+    public int kthSmallestSum(int[] A, int[] B, int k) {
+        // write your code here
+    }
+}
