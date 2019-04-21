477dca19361214fad05873e4cfd7d123d4264ec6
Kaitian Xie <xkaitian@gmail.com>
2019-04-21 15:08:52 -0700

    171: Begin problem

---
+++ src/com/algobot76/lintcode/_171/Solution1.java
@@ -0,0 +1,14 @@
+package com.algobot76.lintcode._171;
+
+import java.util.List;
+
+public class Solution1 {
+    /**
+     * @param strs: A list of strings
+     * @return: A list of strings
+     */
+    public List<String> anagrams(String[] strs) {
+        // write your code here
+
+    }
+}
