60a0e657c24fe4abce5c9072607cfb2150e1912d
Kaitian Xie <xkaitian@gmail.com>
2019-04-21 14:41:09 -0700

    Two Pointers

--- src/com/algobot76/lintcode/_13/Solution1.java
+++ src/com/algobot76/lintcode/_13/Solution1.java
@@ -1,5 +1,8 @@
 package com.algobot76.lintcode._13;
 
+/**
+ * Two Pointers
+ */
 public class Solution1 {
     /**
      * @param source:
@@ -8,5 +11,19 @@ public class Solution1 {
      */
     public int strStr(String source, String target) {
         // Write your code here
+        String curr;
+        int ans = -1;
+
+        for (int l = 0, r = 0; r <= source.length(); r++) {
+            curr = source.substring(l, r);
+            if (curr.equals(target)) {
+                ans = l;
+                break;
+            } else if (curr.length() == target.length()) {
+                l++;
+            }
+        }
+
+        return ans;
     }
 }
