d06a2b43bb8ef201678f4dea9d7206bf28abf221
Kaitian Xie <xkaitian@gmail.com>
2019-04-21 03:17:23 -0700

    543: Priority Queue

---
+++ src/com/algobot76/lintcode/_543/Solution1.java
@@ -0,0 +1,31 @@
+package com.algobot76.lintcode._543;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Priority Queue
+ */
+public class Solution1 {
+    /**
+     * @param arrays: a list of array
+     * @param k:      An integer
+     * @return: an integer, K-th largest element in N arrays
+     */
+    public int KthInArrays(int[][] arrays, int k) {
+        // write your code here
+        int n = arrays.length;
+
+        Queue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < arrays[i].length; j++) {
+                pq.offer(arrays[i][j]);
+                if (pq.size() > k) {
+                    pq.poll();
+                }
+            }
+        }
+
+        return pq.poll();
+    }
+}
