5149cab90dd7f873a984bb8a79cd7706b522a32f
Kaitian Xie <xkaitian@gmail.com>
2019-04-23 07:40:09 -0700

    79: Dynamic Programming

--- src/com/algobot76/lintcode/_79/Solution1.java
+++ src/com/algobot76/lintcode/_79/Solution1.java
@@ -1,5 +1,8 @@
 package com.algobot76.lintcode._79;
 
+/**
+ * Dynamic Programming
+ */
 public class Solution1 {
     /**
      * @param A: A string
@@ -8,5 +11,29 @@ public class Solution1 {
      */
     public int longestCommonSubstring(String A, String B) {
         // write your code here
+        int m = A.length();
+        int n = B.length();
+        int[][] f = new int[m + 1][n + 1];
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (A.charAt(i - 1) == B.charAt(j - 1)) {
+                    f[i][j] = f[i - 1][j - 1] + 1;
+                } else {
+                    f[i][j] = 0;
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (f[i][j] > max) {
+                    max = f[i][j];
+                }
+            }
+        }
+
+        return max;
     }
 }
