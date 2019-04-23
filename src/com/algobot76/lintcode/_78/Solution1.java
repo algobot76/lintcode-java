24980d854e54cd6679c9e1cf2229e37bcd0f5afc
Kaitian Xie <xkaitian@gmail.com>
2019-04-23 08:09:33 -0700

    78: begin problem

---
+++ src/com/algobot76/lintcode/_78/Solution1.java
@@ -0,0 +1,11 @@
+package com.algobot76.lintcode._78;
+
+public class Solution1 {
+    /**
+     * @param strs: A list of strings
+     * @return: The longest common prefix
+     */
+    public String longestCommonPrefix(String[] strs) {
+        // write your code here
+    }
+}
