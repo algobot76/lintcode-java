3f6f5f1a924f37116a85c5a3915d6fbaf6f9af61
Kaitian Xie <xkaitian@gmail.com>
2019-04-23 08:21:09 -0700

    78: Horizontal Scanning

--- src/com/algobot76/lintcode/_78/Solution1.java
+++ src/com/algobot76/lintcode/_78/Solution1.java
@@ -1,5 +1,8 @@
 package com.algobot76.lintcode._78;
 
+/**
+ * Horizontal Scanning
+ */
 public class Solution1 {
     /**
      * @param strs: A list of strings
@@ -7,5 +10,20 @@ public class Solution1 {
      */
     public String longestCommonPrefix(String[] strs) {
         // write your code here
+        if (strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.length() == 0) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
     }
 }
