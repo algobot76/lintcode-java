6b2b794c9b48825c58477126023defd4ca5a4207
Kaitian Xie <xkaitian@gmail.com>
2019-04-23 10:29:38 -0700

    172: begin problem

---
+++ src/com/algobot76/lintcode/_172/Solution1.java
@@ -0,0 +1,12 @@
+package com.algobot76.lintcode._172;
+
+public class Solution1 {
+    /*
+     * @param A: A list of integers
+     * @param elem: An integer
+     * @return: The new length after remove
+     */
+    public int removeElement(int[] A, int elem) {
+        // write your code here
+    }
+}
