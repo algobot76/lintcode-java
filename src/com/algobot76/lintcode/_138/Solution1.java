c854f5b20d311f17d3812c17c4be4e0eff3581ba
Kaitian Xie <xkaitian@gmail.com>
2019-04-23 12:04:51 -0700

    138: Hash Table

--- src/com/algobot76/lintcode/_138/Solution1.java
+++ src/com/algobot76/lintcode/_138/Solution1.java
@@ -1,6 +1,9 @@
 package com.algobot76.lintcode._138;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Hash Table
@@ -12,5 +15,24 @@ public class Solution1 {
      */
     public List<Integer> subarraySum(int[] nums) {
         // write your code here
+        List<Integer> ans = new ArrayList<>();
+        if (nums.length == 0) {
+            return ans;
+        }
+
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum)) {
+                ans.add(map.get(sum) + 1);
+                ans.add(i);
+                return ans;
+            }
+            map.put(sum, i);
+        }
+
+        return ans;
     }
 }
