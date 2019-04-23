426f3ffb5ec60e2804da059bbf566df1809865b5
Kaitian Xie <xkaitian@gmail.com>
2019-04-23 11:00:39 -0700

    138: begin problem

---
+++ src/com/algobot76/lintcode/_138/Solution1.java
@@ -0,0 +1,16 @@
+package com.algobot76.lintcode._138;
+
+import java.util.List;
+
+/**
+ * Hash Table
+ */
+public class Solution1 {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number and the index of the last number
+     */
+    public List<Integer> subarraySum(int[] nums) {
+        // write your code here
+    }
+}
