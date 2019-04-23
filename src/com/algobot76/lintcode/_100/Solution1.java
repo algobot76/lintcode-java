15aaed313a27b2aef5dfadeb48afce0feb62e942
Kaitian Xie <xkaitian@gmail.com>
2019-04-23 12:27:52 -0700

    100: Two Pointers

--- src/com/algobot76/lintcode/_100/Solution1.java
+++ src/com/algobot76/lintcode/_100/Solution1.java
@@ -1,5 +1,8 @@
 package com.algobot76.lintcode._100;
 
+/**
+ * Two Pointers
+ */
 public class Solution1 {
     /*
      * @param nums: An ineger array
@@ -7,5 +10,17 @@ public class Solution1 {
      */
     public int removeDuplicates(int[] nums) {
         // write your code here
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int size = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[size]) {
+                nums[++size] = nums[i];
+            }
+        }
+
+        return size + 1;
     }
 }
