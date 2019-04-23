c80a89848d32f7e4cc487192298f1bcbda109744
Kaitian Xie <xkaitian@gmail.com>
2019-04-23 12:11:46 -0700

    100: begin problem

---
+++ src/com/algobot76/lintcode/_100/Solution1.java
@@ -0,0 +1,11 @@
+package com.algobot76.lintcode._100;
+
+public class Solution1 {
+    /*
+     * @param nums: An ineger array
+     * @return: An integer
+     */
+    public int removeDuplicates(int[] nums) {
+        // write your code here
+    }
+}
