9ffa299de3d9bfb4e4d561788db2932e4d98cac0
Kaitian Xie <xkaitian@gmail.com>
2019-04-23 07:33:18 -0700

    79: begin problem

---
+++ src/com/algobot76/lintcode/_79/Solution1.java
@@ -0,0 +1,12 @@
+package com.algobot76.lintcode._79;
+
+public class Solution1 {
+    /**
+     * @param A: A string
+     * @param B: A string
+     * @return: the length of the longest common substring.
+     */
+    public int longestCommonSubstring(String A, String B) {
+        // write your code here
+    }
+}
