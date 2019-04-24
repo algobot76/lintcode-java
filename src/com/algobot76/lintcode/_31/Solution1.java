e04ce7e1b4ca09a0cc509829c515d0bd39476ca5
Kaitian Xie <xkaitian@gmail.com>
2019-04-24 15:25:12 -0700

    31: begin problem

---
+++ src/com/algobot76/lintcode/_31/Solution1.java
@@ -0,0 +1,12 @@
+package com.algobot76.lintcode._31;
+
+public class Solution1 {
+    /**
+     * @param nums: The integer array you should partition
+     * @param k:    An integer
+     * @return: The index after partition
+     */
+    public int partitionArray(int[] nums, int k) {
+        // write your code here
+    }
+}
