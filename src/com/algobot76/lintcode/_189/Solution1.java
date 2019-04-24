75a0806e3f158b42077db1c67c03262747710a24
Kaitian Xie <xkaitian@gmail.com>
2019-04-24 13:21:26 -0700

    189: begin problem

---
+++ src/com/algobot76/lintcode/_189/Solution1.java
@@ -0,0 +1,11 @@
+package com.algobot76.lintcode._189;
+
+public class Solution1 {
+    /**
+     * @param A: An array of integers
+     * @return: An integer
+     */
+    public int firstMissingPositive(int[] A) {
+        // write your code here
+    }
+}
