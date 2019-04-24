fc7e3df0d8deac4e5832dd79393fa450a12e10b7
Kaitian Xie <xkaitian@gmail.com>
2019-04-24 15:32:33 -0700

    31: Quick Select

--- src/com/algobot76/lintcode/_31/Solution1.java
+++ src/com/algobot76/lintcode/_31/Solution1.java
@@ -1,5 +1,8 @@
 package com.algobot76.lintcode._31;
 
+/**
+ * Quick Select
+ */
 public class Solution1 {
     /**
      * @param nums: The integer array you should partition
@@ -8,5 +11,28 @@ public class Solution1 {
      */
     public int partitionArray(int[] nums, int k) {
         // write your code here
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int l = 0;
+        int r = nums.length - 1;
+        while (l <= r) {
+            while (l <= r && nums[l] < k) {
+                l++;
+            }
+            while (l <= r && nums[r] >= k) {
+                r--;
+            }
+            if (l <= r) {
+                int temp = nums[l];
+                nums[l] = nums[r];
+                nums[r] = temp;
+                l++;
+                r--;
+            }
+        }
+
+        return l;
     }
 }
