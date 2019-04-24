fa67c1c9bd19e841c44d90ef1add388548b3a094
Kaitian Xie <xkaitian@gmail.com>
2019-04-24 15:02:22 -0700

    189: finish problem

--- src/com/algobot76/lintcode/_189/Solution1.java
+++ src/com/algobot76/lintcode/_189/Solution1.java
@@ -7,5 +7,23 @@ public class Solution1 {
      */
     public int firstMissingPositive(int[] A) {
         // write your code here
+        int len = A.length;
+        for (int i = 0; i < len; i++) {
+            A[i]--;
+        }
+        for (int i = 0; i < len; i++) {
+            while (A[i] != i && A[i] >= 0 && A[i] < len && A[i] != A[A[i]]) {
+                int temp = A[i];
+                A[i] = A[A[i]];
+                A[temp] = temp;
+            }
+        }
+        for (int i = 0; i < len; i++) {
+            if (A[i] != i) {
+                return i + 1;
+            }
+        }
+
+        return len + 1;
     }
 }
