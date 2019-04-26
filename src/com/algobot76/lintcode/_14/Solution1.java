9e1d9c622b7eefcf744548e63815e2a80ab43226
Kaitian Xie <xkaitian@gmail.com>
2019-04-26 10:01:46 -0700

    14: Binary Search

---
+++ src/com/algobot76/lintcode/_14/Solution1.java
@@ -0,0 +1,33 @@
+package com.algobot76.lintcode._14;
+
+/**
+ * Binary Search
+ */
+public class Solution1 {
+    /**
+     * @param nums:   The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+        // write your code here
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                if (mid == left || nums[mid - 1] != target) {
+                    return mid;
+                } else {
+                    right = mid - 1;
+                }
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
