5974b2bcd1376bd25392bddf3f25ca02a84b9652
Kaitian Xie <xkaitian@gmail.com>
2019-04-27 11:53:34 -0700

    181: begin problem

---
+++ src/com/algobot76/lintcode/_181/Solution1.java
@@ -0,0 +1,12 @@
+package com.algobot76.lintcode._181;
+
+public class Solution1 {
+    /**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: An integer
+     */
+    public int bitSwapRequired(int a, int b) {
+        // write your code here
+    }
+}
