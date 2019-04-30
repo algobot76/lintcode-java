f8188581518c06efd92a6497fcd6670dc8b0fb2f
Kaitian Xie <xkaitian@gmail.com>
2019-04-30 14:04:09 -0700

    591: begin problem

---
+++ src/com/algobot76/lintcode/_591/ConnectingGraph3.java
@@ -0,0 +1,19 @@
+package com.algobot76.lintcode._591;
+
+public class ConnectingGraph3 {
+    /**
+     * @param a: An integer
+     * @param b: An integer
+     * @return: nothing
+     */
+    public void connect(int a, int b) {
+        // write your code here
+    }
+
+    /**
+     * @return: An integer
+     */
+    public int query() {
+        // write your code here
+    }
+}
