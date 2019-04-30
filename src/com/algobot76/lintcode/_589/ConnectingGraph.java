7eb0a0c4abacee5bceb831e67ec73dd35b58e7e3
Kaitian Xie <xkaitian@gmail.com>
2019-04-30 11:15:44 -0700

    589: begin problem

---
+++ src/com/algobot76/lintcode/_589/ConnectingGraph.java
@@ -0,0 +1,28 @@
+package com.algobot76.lintcode._589;
+
+public class ConnectingGraph {
+    /*
+     * @param n: An integer
+     */
+    public ConnectingGraph(int n) {
+        // do intialization if necessary
+    }
+
+    /*
+     * @param a: An integer
+     * @param b: An integer
+     * @return: nothing
+     */
+    public void connect(int a, int b) {
+        // write your code here
+    }
+
+    /*
+     * @param a: An integer
+     * @param b: An integer
+     * @return: A boolean
+     */
+    public boolean query(int a, int b) {
+        // write your code here
+    }
+}
