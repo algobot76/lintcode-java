72859deeafbddc9ddddb88ed26d5567d17cdc261
Kaitian Xie <xkaitian@gmail.com>
2019-04-30 13:16:48 -0700

    589: Union Find

--- src/com/algobot76/lintcode/_589/ConnectingGraph.java
+++ src/com/algobot76/lintcode/_589/ConnectingGraph.java
@@ -1,11 +1,20 @@
 package com.algobot76.lintcode._589;
 
+/**
+ * Union Find
+ */
 public class ConnectingGraph {
+    private int[] parents;
+
     /*
      * @param n: An integer
      */
     public ConnectingGraph(int n) {
         // do intialization if necessary
+        parents = new int[n];
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+        }
     }
 
     /*
@@ -15,6 +24,11 @@ public class ConnectingGraph {
      */
     public void connect(int a, int b) {
         // write your code here
+        int parentA = find(--a);
+        int parentB = find(--b);
+        if (parentA != parentB) {
+            parents[parentA] = parentB;
+        }
     }
 
     /*
@@ -24,5 +38,21 @@ public class ConnectingGraph {
      */
     public boolean query(int a, int b) {
         // write your code here
+        return find(--a) == find(--b);
+    }
+
+    private int find(int x) {
+        int p = x;
+
+        while (parents[p] != p) {
+            p = parents[p];
+        }
+        while (x != p) {
+            int temp = parents[x];
+            parents[x] = p;
+            x = temp;
+        }
+
+        return p;
     }
 }
