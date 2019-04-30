e500aa7e150f35e1ec7aa12c1467fef7c1aee2a2
Kaitian Xie <xkaitian@gmail.com>
2019-04-30 13:57:34 -0700

    590: wip

--- src/com/algobot76/lintcode/_590/ConnectingGraph2.java
+++ src/com/algobot76/lintcode/_590/ConnectingGraph2.java
@@ -1,11 +1,23 @@
 package com.algobot76.lintcode._590;
 
+/**
+ * Union Find
+ */
 public class ConnectingGraph2 {
+    private int[] parents;
+    private int[] count;
+
     /*
      * @param n: An integer
      */
     public ConnectingGraph2(int n) {
         // do intialization if necessary
+        parents = new int[n];
+        count = new int[n];
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+            count[i] = 1;
+        }
     }
 
     /*
@@ -15,6 +27,12 @@ public class ConnectingGraph2 {
      */
     public void connect(int a, int b) {
         // write your code here
+        int parentA = find(a);
+        int parentB = find(b);
+        if (parentA != parentB) {
+            parents[parentA] = parentB;
+            count[parentB] += count[parentA];
+        }
     }
 
     /*
@@ -23,5 +41,22 @@ public class ConnectingGraph2 {
      */
     public int query(int a) {
         // write your code here
+        int parent = find(a);
+        return count[parent];
+    }
+
+    private int find(int x) {
+        int p = x - 1;
+
+        while (parents[p] != p) {
+            p = parents[p];
+        }
+        while (x != p) {
+            int temp = parents[x];
+            parents[x] = p;
+            x = temp;
+        }
+
+        return p;
     }
 }
