8f47db4708192f5c8377f9bbaeab21330298c2dc
Kaitian Xie <xkaitian@gmail.com>
2019-05-04 17:10:30 -0700

    629: begin problem

---
+++ src/com/algobot76/lintcode/_629/Solution1.java
@@ -0,0 +1,15 @@
+package com.algobot76.lintcode._629;
+
+import com.algobot76.lintcode.util.Connection;
+
+import java.util.List;
+
+public class Solution1 {
+    /**
+     * @param connections given a list of connections include two cities and cost
+     * @return a list of connections from results
+     */
+    public List<Connection> lowestCost(List<Connection> connections) {
+        // Write your code here
+    }
+}
---
+++ src/com/algobot76/lintcode/util/Connection.java
@@ -0,0 +1,13 @@
+package com.algobot76.lintcode.util;
+
+public class Connection {
+    public String city1;
+    public String city2;
+    public int cost;
+
+    public Connection(String city1, String city2, int cost) {
+        this.city1 = city1;
+        this.city2 = city2;
+        this.cost = cost;
+    }
+}
