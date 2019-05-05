7ecc020bd97fc6a7e5870f9c67d462b0aa6b23fc
Kaitian Xie <xkaitian@gmail.com>
2019-05-05 10:36:07 -0700

    81: Priority Queue

--- src/com/algobot76/lintcode/_81/Solution1.java
+++ src/com/algobot76/lintcode/_81/Solution1.java
@@ -1,5 +1,12 @@
 package com.algobot76.lintcode._81;
 
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * Priority Queue
+ */
 public class Solution1 {
     /**
      * @param nums: A list of integers
@@ -7,5 +14,33 @@ public class Solution1 {
      */
     public int[] medianII(int[] nums) {
         // write your code here
+        int n = nums.length;
+        int[] ans = new int[n];
+        int count = 0;
+        Queue<Integer> maxHeap = new PriorityQueue<>(n, Collections.reverseOrder());
+        Queue<Integer> minHeap = new PriorityQueue<>(n);
+
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            maxHeap.add(num);
+            if (count % 2 == 0) {
+                if (minHeap.isEmpty()) {
+                    count++;
+                    ans[i] = maxHeap.peek();
+                    continue;
+                } else if (maxHeap.peek() > minHeap.peek()) {
+                    int maxHeapRoot = maxHeap.poll();
+                    int minHeapRoot = minHeap.poll();
+                    maxHeap.add(minHeapRoot);
+                    minHeap.add(maxHeapRoot);
+                }
+            } else {
+                minHeap.add(maxHeap.poll());
+            }
+            count++;
+            ans[i] = maxHeap.peek();
+        }
+
+        return ans;
     }
 }
