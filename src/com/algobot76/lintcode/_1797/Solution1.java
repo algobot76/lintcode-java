46b656d3a09e42da8c42b418a947cd829c0723ac
Kaitian Xie <xkaitian@gmail.com>
2019-05-25 10:41:00 -0700

    1797: begin problem

---
+++ src/com/algobot76/lintcode/_1797/Solution1.java
@@ -0,0 +1,13 @@
+package com.algobot76.lintcode._1797;
+
+public class Solution1 {
+    /**
+     * @param A: a integer sorted array
+     * @param B: a integer sorted array
+     * @param K: a integer
+     * @return: return a pair of index
+     */
+    public int[] optimalUtilization(int[] A, int[] B, int K) {
+        // write your code here
+    }
+}
