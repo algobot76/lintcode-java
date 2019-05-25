09a6d9dfd16058ed6d6dfcad6c237e0020bb33c3
Kaitian Xie <xkaitian@gmail.com>
2019-05-25 12:36:49 -0700

    1797: finish problem

--- src/com/algobot76/lintcode/_1797/Solution1.java
+++ src/com/algobot76/lintcode/_1797/Solution1.java
@@ -8,6 +8,33 @@ public class Solution1 {
      * @return: return a pair of index
      */
     public int[] optimalUtilization(int[] A, int[] B, int K) {
-        // write your code here
+        if (A.length == 0 || B.length == 0) {
+            return new int[0];
+        }
+
+        int ansL = 0;
+        int ansR = B.length - 1;
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = B.length - 1; j >= 0; j--) {
+                int sum = A[i] + B[j];
+                if (sum <= K) {
+                    if (sum > maxSum) {
+                        ansL = i;
+                        ansR = j;
+                        maxSum = sum;
+                    } else if (sum == maxSum) {
+                        if (i < ansL) {
+                            ansL = i;
+                            ansR = j;
+                        } else if (i == ansL) {
+                            ansR = j;
+                        }
+                    }
+                }
+            }
+        }
+
+        return new int[]{ansL, ansR};
     }
 }
