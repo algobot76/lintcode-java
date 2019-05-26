88160b9ec28e2e2806bb0c6ebb233ff86c2d3b09
Kaitian Xie <xkaitian@gmail.com>
2019-05-26 15:58:16 -0700

    613: finish problem

--- src/com/algobot76/lintcode/_613/Solution1.java
+++ src/com/algobot76/lintcode/_613/Solution1.java
@@ -2,7 +2,10 @@ package com.algobot76.lintcode._613;
 
 import com.algobot76.lintcode.util.Record;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class Solution1 {
     /**
@@ -11,6 +14,29 @@ public class Solution1 {
      * Map<Integer, Double> (student_id, average_score)
      */
     public Map<Integer, Double> highFive(Record[] results) {
-        // Write your code here
+        Map<Integer, Double> ans = new HashMap<>();
+        if (results == null || results.length == 0) {
+            return ans;
+        }
+
+        Map<Integer, Queue<Integer>> map = new HashMap<>();
+        for (Record record : results) {
+            Queue<Integer> pq = map.getOrDefault(record.id, new PriorityQueue<>());
+            pq.offer(record.score);
+            if (pq.size() > 5) {
+                pq.poll();
+            }
+            map.put(record.id, pq);
+        }
+        map.forEach((id, pq) -> {
+            double avg = 0;
+            while (!pq.isEmpty()) {
+                avg += pq.poll();
+            }
+            avg /= 5;
+            ans.put(id, avg);
+        });
+
+        return ans;
     }
 }
