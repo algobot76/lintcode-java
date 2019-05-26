3d40969c02bf05ba4184bf152e4520cd4d2ca8f8
Kaitian Xie <xkaitian@gmail.com>
2019-05-26 09:36:16 -0700

    1639: begin problem

---
+++ src/com/algobot76/lintcode/_1639/Solution1.java
@@ -0,0 +1,12 @@
+package com.algobot76.lintcode._1639;
+
+public class Solution1 {
+    /**
+     * @param stringIn: The original string.
+     * @param K:        The length of substrings.
+     * @return: return the count of substring of length K and exactly K distinct characters.
+     */
+    public int KSubstring(String stringIn, int K) {
+        // Write your code here
+    }
+}
