59740985ec5af82c371c9d743b5db2fa6264e25c
Kaitian Xie <xkaitian@gmail.com>
2019-05-25 17:47:30 -0700

    1577: begin problem

---
+++ src/com/algobot76/lintcode/_1577/Solution1.java
@@ -0,0 +1,13 @@
+package com.algobot76.lintcode._1577;
+
+import com.algobot76.lintcode.util.TreeNode;
+
+public class Solution1 {
+    /**
+     * @param root:
+     * @return: the sum of leafnode
+     */
+    public int sumLeafNode(TreeNode root) {
+        // Write your code here
+    }
+}
---
+++ src/com/algobot76/lintcode/util/TreeNode.java
@@ -0,0 +1,11 @@
+package com.algobot76.lintcode.util;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        this.val = x;
+    }
+}
