b18d9c204b4eb5e4e8a08bcbe59e260359a8ee79
Kaitian Xie <xkaitian@gmail.com>
2019-05-26 10:23:46 -0700

    1639: Two Pointers

--- src/com/algobot76/lintcode/_1639/Solution1.java
+++ src/com/algobot76/lintcode/_1639/Solution1.java
@@ -1,5 +1,11 @@
 package com.algobot76.lintcode._1639;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Two Pointers
+ */
 public class Solution1 {
     /**
      * @param stringIn: The original string.
@@ -7,6 +13,27 @@ public class Solution1 {
      * @return: return the count of substring of length K and exactly K distinct characters.
      */
     public int KSubstring(String stringIn, int K) {
-        // Write your code here
+        if (stringIn == null || stringIn.length() == 0 || stringIn.length() < K) {
+            return 0;
+        }
+
+        Set<Character> charSet = new HashSet<>();
+        Set<String> resSet = new HashSet<>();
+        for (int i = 0, j = 0; i < stringIn.length(); i++) {
+            while (j < stringIn.length() && charSet.size() < K) {
+                char ch = stringIn.charAt(j);
+                if (charSet.contains(ch)) {
+                    break;
+                }
+                charSet.add(ch);
+                if (charSet.size() == K) {
+                    resSet.add(stringIn.substring(i, j + 1));
+                }
+                j++;
+            }
+            charSet.remove(stringIn.charAt(i));
+        }
+
+        return resSet.size();
     }
 }
