684fb00682ac79d5786bb56f4c825497cead424d
Kaitian Xie <xkaitian@gmail.com>
2019-05-26 15:04:10 -0700

    613: begin problem

---
+++ src/com/algobot76/lintcode/_613/Solution1.java
@@ -0,0 +1,16 @@
+package com.algobot76.lintcode._613;
+
+import com.algobot76.lintcode.util.Record;
+
+import java.util.Map;
+
+public class Solution1 {
+    /**
+     * @param results a list of <student_id, score>
+     * @return find the average of 5 highest scores for each person
+     * Map<Integer, Double> (student_id, average_score)
+     */
+    public Map<Integer, Double> highFive(Record[] results) {
+        // Write your code here
+    }
+}
---
+++ src/com/algobot76/lintcode/util/Record.java
@@ -0,0 +1,10 @@
+package com.algobot76.lintcode.util;
+
+public class Record {
+    public int id;
+    public int score;
+    public Record(int id, int score) {
+        this.id = id;
+        this.score = score;
+    }
+}
