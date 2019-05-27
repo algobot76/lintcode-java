ad2759260589a84566adc7ccae294468a8ea7375
Kaitian Xie <xkaitian@gmail.com>
2019-05-26 17:21:27 -0700

    597: begin problem

---
+++ src/com/algobot76/lintcode/_597/Solution1.java
@@ -0,0 +1,13 @@
+package com.algobot76.lintcode._597;
+
+import com.algobot76.lintcode.util.TreeNode;
+
+public class Solution1 {
+    /**
+     * @param root: the root of binary tree
+     * @return: the root of the maximum average of subtree
+     */
+    public TreeNode findSubtree2(TreeNode root) {
+        // write your code here
+    }
+}
