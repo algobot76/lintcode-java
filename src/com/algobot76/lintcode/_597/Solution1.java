fc95d6ad8e2250a0278d39e414ab3c51d5bb5470
Kaitian Xie <xkaitian@gmail.com>
2019-05-26 17:34:54 -0700

    597: finish problem

--- src/com/algobot76/lintcode/_597/Solution1.java
+++ src/com/algobot76/lintcode/_597/Solution1.java
@@ -3,11 +3,31 @@ package com.algobot76.lintcode._597;
 import com.algobot76.lintcode.util.TreeNode;
 
 public class Solution1 {
+    private TreeNode res = null;
+    private double maxAvg = (double) Integer.MIN_VALUE;
+
     /**
      * @param root: the root of binary tree
      * @return: the root of the maximum average of subtree
      */
     public TreeNode findSubtree2(TreeNode root) {
-        // write your code here
+        helper(root, 0, 0);
+        return res;
+    }
+
+    private int[] helper(TreeNode node, int sum, int size) {
+        if (node == null) {
+            return new int[]{0, 0};
+        }
+        int[] resL = helper(node.left, sum, size);
+        int[] resR = helper(node.right, sum, size);
+        int currSum = resL[0] + resR[0] + node.val;
+        int currSize = resL[1] + resR[1] + 1;
+        double avg = ((double) currSum) / currSize;
+        if (avg > maxAvg) {
+            maxAvg = avg;
+            res = node;
+        }
+        return new int[]{currSum, currSize};
     }
 }
