99bf571f8c0239c3576a9d585055762b5b217c03
Kaitian Xie <xkaitian@gmail.com>
2019-11-18 19:26:50 -0800

    533: Solve problem using two pointers

--- src/com/algobot76/lintcode/_533/Solution1.java
+++ src/com/algobot76/lintcode/_533/Solution1.java
@@ -1,12 +1,30 @@
 package com.algobot76.lintcode._533;
 
+import java.util.Arrays;
+
 public class Solution1 {
     /**
-     * @param nums: an integer array
+     * @param nums:   an integer array
      * @param target: An integer
      * @return: the difference between the sum and the target
      */
     public int twoSumClosest(int[] nums, int target) {
-        // write your code here
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+
+        int diff = Integer.MAX_VALUE;
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+            diff = Math.min(diff, Math.abs(sum - target));
+        }
+
+        return diff;
+
     }
 }
