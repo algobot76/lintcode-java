24eb4629d6a58491bd36f45aa916e2295d6cdc20
Kaitian Xie <xkaitian@gmail.com>
2019-11-18 19:23:02 -0800

    533: Begin problem

---
+++ src/com/algobot76/lintcode/_533/Solution1.java
@@ -0,0 +1,12 @@
+package com.algobot76.lintcode._533;
+
+public class Solution1 {
+    /**
+     * @param nums: an integer array
+     * @param target: An integer
+     * @return: the difference between the sum and the target
+     */
+    public int twoSumClosest(int[] nums, int target) {
+        // write your code here
+    }
+}
